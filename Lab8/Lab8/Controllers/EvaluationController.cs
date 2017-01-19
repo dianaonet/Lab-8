81705e9926b2a0dd2709e992fe1d5646b19a2302
Diana Onet <Diana Onet>
2017-01-19 23:01:53 +0200

    Lab8 Ex1

---
+++ Lab8/Lab8/Controllers/EvaluationController.cs
@@ -0,0 +1,96 @@
+﻿using Lab8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab8.Controllers
+{
+    public class EvaluationController : Controller
+    {
+        public static List<ProjectEvaluation> ListOfProject = new List<ProjectEvaluation>
+        {
+            new ProjectEvaluation() {name="Diana", city = "Cluj", country="Romania", rating= 9.5},
+            new ProjectEvaluation() {name="Maria", city = "Campeni City", country="Romania", rating= 7.34},
+            new ProjectEvaluation() {name="Razvan", city = "Sibiu", country="Romania", rating= 8.8}
+        };
+        // GET: Evaluation
+        public ActionResult Index()
+        {
+            return View(ListOfProject);
+        }
+
+        // GET: Evaluation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Evaluation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Evaluation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Evaluation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Evaluation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab8/Lab8/Models/ProjectEvaluation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab8.Models
+{
+    public class ProjectEvaluation
+    {
+        public string name { get; set; }
+      
+        public string city { get; set; }
+
+        public string country { get; set; }
+
+        public double rating { get; set; }
+    }
+}
